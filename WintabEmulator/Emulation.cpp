9af0d2af03b9d87acc747c1f48b0c74425b9f86d
Carl Ritson <carl@ritson.email>
2020-07-11 10:25:18 +0900

    Relicense under MIT license.

--- WintabEmulator/Emulation.cpp
+++ WintabEmulator/Emulation.cpp
@@ -1,10 +1,24 @@
 /*------------------------------------------------------------------------------
 WintabEmulator - Emulation.cpp
-Copyright (c) 2013 Carl G. Ritson <critson@perlfu.co.uk>
+Copyright (c) 2013, 2014, 2020 Carl G. Ritson <critson@perlfu.co.uk>
 
-This file may be freely used, copied, or distributed without compensation 
-or licensing restrictions, but is done so without any warranty or implication
-of merchantability or fitness for any particular purpose.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 ------------------------------------------------------------------------------*/
 
 #include "stdafx.h"
@@ -136,11 +150,11 @@ static BOOL update_screen_metrics(LOGCONTEXTA *ctx)
         ctx->lcOutExtX  = ctx->lcInExtX;
         ctx->lcOutExtY  = ctx->lcInExtY;
         ctx->lcOutExtZ  = ctx->lcInExtZ;
-		
-		ctx->lcSysOrgX  = ctx->lcInOrgX;
-		ctx->lcSysOrgY  = ctx->lcInOrgY;
-		ctx->lcSysExtX  = ctx->lcInExtX;
-		ctx->lcSysExtY  = ctx->lcInExtY;
+
+        ctx->lcSysOrgX  = ctx->lcInOrgX;
+        ctx->lcSysOrgY  = ctx->lcInOrgY;
+        ctx->lcSysExtX  = ctx->lcInExtX;
+        ctx->lcSysExtY  = ctx->lcInExtY;
     }
 
     return changed;
